b5aa5fd410b13a4f234586720bc526664764ce24
focusberbboo <61118960+wimon09@users.noreply.github.com>
2025-09-22 19:55:44 +0700

    Invoice.java

---
+++ Invoice.java
@@ -0,0 +1,95 @@
+import java.util.*;
+
+public class Invoice {
+	private String invoiceID;
+	private Date date;
+	private String signature;
+	private double tradeInAllowance;
+	private double finalPrice;
+	private double tax;
+	private double licenseFee;
+
+	private customer customer; // One customer per invoice
+	private Salesperson salesperson; // One salesperson per invoice
+	private Vehicle vehicle; // One vehicle per invoice
+	private TradeInVehicle tradeInVehicle; // Optional trade-in vehicle
+	private ArrayList<Option> options = new ArrayList<>(); // Many options possible
+
+	public Invoice(String invoiceID, Date date, String signature, double tradeInAllowance, double finalPrice,
+			double tax, double licenseFee, customer customer, Salesperson salesperson, Vehicle vehicle) {
+		this.invoiceID = invoiceID;
+		this.date = date;
+		this.signature = signature;
+		this.tradeInAllowance = tradeInAllowance;
+		this.finalPrice = finalPrice;
+		this.tax = tax;
+		this.licenseFee = licenseFee;
+		this.customer = customer;
+		this.salesperson = salesperson;
+		this.vehicle = vehicle;
+
+		// maintain bidirectional links
+		customer.addInvoice(this);
+		salesperson.addInvoice(this);
+		vehicle.setInvoice(this);
+	}
+
+	// Getters and setters
+	public String getInvoiceID() {
+		return invoiceID;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public String getSignature() {
+		return signature;
+	}
+
+	public double getTradeInAllowance() {
+		return tradeInAllowance;
+	}
+
+	public double getFinalPrice() {
+		return finalPrice;
+	}
+
+	public double getTax() {
+		return tax;
+	}
+
+	public double getLicenseFee() {
+		return licenseFee;
+	}
+
+	public customer getCustomer() {
+		return customer;
+	}
+
+	public Salesperson getSalesperson() {
+		return salesperson;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public TradeInVehicle getTradeInVehicle() {
+		return tradeInVehicle;
+	}
+
+	public List<Option> getOptions() {
+		return options;
+	}
+
+	public void setTradeInVehicle(TradeInVehicle tradeInVehicle) {
+		this.tradeInVehicle = tradeInVehicle;
+		tradeInVehicle.addInvoice(this);
+	}
+
+	public void addOption(Option option) {
+		options.add(option);
+		option.addInvoice(this);
+	}
+}
